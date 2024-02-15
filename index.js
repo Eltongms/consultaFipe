3d58f24ae72bdcfec3eba0e888047fc67895db19
Elton Peruzzo Gomes <epggpe@gmail.com>
2024-02-15 17:06:11 -0300

    tentado listar anos com form sem sucesso

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import bodyParser from "body-parser"
 const app = express()
 const port = 3000
 let marcas
+let marcaEscolhida
+let modelos
+let modeloEscolhido
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -31,33 +34,50 @@ app.get('/modelos', async (req, res) => {
     let codigoModelo = 0;
 
     if (marcas) {
-        let marcaSelecionada = req.query.marca
-        codigoModelo = marcas.find((e) => e.nome === marcaSelecionada).codigo
+        // let marcaSelecionada = req.query.marca
+        marcaEscolhida = marcas.find((e) => e.nome === req.query.marca)
     } else {
-       res.redirect("/")
+        res.redirect("/")
         return
     }
     try {
-        const result = await axios.get("https://parallelum.com.br/fipe/api/v1/carros/marcas/" + codigoModelo + "/modelos");
-        let modelos = [];
-        console.log(result.data)
-        result.data.modelos.forEach(element => {
-            modelos.push(element.nome)
+        // console.log(marcaEscolhida)
+        const result = await axios.get("https://parallelum.com.br/fipe/api/v1/carros/marcas/" + marcaEscolhida.codigo + "/modelos");
+        modelos = result.data
+        // console.log(modelos.modelos)
+
+        let modelosNomes = [];
+        modelos.modelos.forEach(element => {
+            modelosNomes.push(element.nome)
         });
+        // console.log(modelosNomes)
 
-        res.render("index.ejs",{marcas: marcas ,modelos: modelos})
+        res.render("index.ejs", { marcas: marcas, modelos: modelosNomes })
 
-       //  res.send(modelos);
+        //  res.send(modelos);
     } catch (error) {
         if (error.response) {
             res.send("Erro de resposta ao buscar modelos: " + error.response.statusText)
         } else {
-            res.send("Erro de request buscar modelos: " + error.request)
+            res.send("Erro de request buscar modelos: " + error)
         }
-    }    
+    }
 })
 
+app.get('/anos',  (req, res) => {
 
+    // req.query.modelo
+
+    modeloEscolhido = modelos.find((e) => e.nome === req.query.modelo);
+
+    let anos = [200, 255, 20];
+
+    console.log("saida " + anos);
+
+    res.render("index.ejs", { marcas: marcas, modelos: modelosNomes, anos: anos } )
+
+    // const result = await axios.get("")
+})
 
 app.listen(port, () => {
     console.log(`Runnig and listning on port ${port}`)
