ed04d19037df280bc9362c1279eb93dcf9be8c1d
Elton Peruzzo Gomes <epggpe@gmail.com>
2024-02-15 11:40:29 -0300

    modelos listados

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 import express from "express"
 import ejs from "ejs"
-import axios from "axios";
+import axios from "axios"
+import bodyParser from "body-parser"
 
 const app = express()
 const port = 3000
+let marcas
 
-let marcas;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
+
 
 app.get('/', async (req, res) => {
 
-    const result = await axios.get("https://parallelum.com.br/fipe/api/v1/carros/marcas");
-    marcas = result;
-
-    res.render("index.ejs", { marcas: result.data })
+    let result = null;
+    try {
+        result = await axios.get("https://parallelum.com.br/fipe/api/v1/carros/marcas")
+        marcas = result.data
+        res.render("index.ejs", { marcas: marcas })
+    } catch (error) {
+        res.send("Erro ao buscar marcas " + error.response)
+    }
 })
 
 
-app.get('/modelos', (req, res) => {
+app.get('/modelos', async (req, res) => {
 
+    // let saida = marcas
+    let codigoModelo = 0;
 
-    console.log(req.body.marca)
+    if (marcas) {
+        let marcaSelecionada = req.query.marca
+        codigoModelo = marcas.find((e) => e.nome === marcaSelecionada).codigo
+    } else {
+       res.redirect("/")
+        return
+    }
+    try {
+        const result = await axios.get("https://parallelum.com.br/fipe/api/v1/carros/marcas/" + codigoModelo + "/modelos");
+        let modelos = [];
+        console.log(result.data)
+        result.data.modelos.forEach(element => {
+            // modelos.push(element.nome)
+            modelos.push(element.nome)
+        });
+        res.send(modelos);
+    } catch (error) {
+        if (error.response) {
+            res.send("Erro ao buscar modelos: " + error.response.statusText)
+        } else {
+            res.send("Erro ao buscar modelos: " + error.request)
+        }
+    }
 
+    // res.render("index.ejs",)
 
-    //const result = await axios.get("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+);
 
     //res.render("index.ejs", {marcas: result.data})
 })
